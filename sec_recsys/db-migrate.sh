b2c7605822aecf9d25a63d80450be1a20db8aed7
Minh Tu <tungominh.hust@gmail.com>
2025-01-12 23:59:00 +0700

    Remove SecRecsys

--- sec_recsys/db-migrate.sh
+++
@@ -1,8 +0,0 @@
-
-docker compose run web python manage.py makemigrations --noinput
-docker compose run web python manage.py migrate --run-syncdb
-
-docker compose run web python populate_moviegeek.py
-docker compose run web python populate_ratings.py
-# docker compose run web python populate_sample_of_descriptions.py
-docker compose run web python populate_logs.py
--- sec_recsys/moviegeeks/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from moviegeeks import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('movie/<int:movie_id>/', views.detail, name='detail'),
-    path('genre/<str:genre_id>/', views.genre, name='genre'),
-    path('search/', views.search_for_movie, name='search_for_movie'),
-]
